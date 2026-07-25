7b2c03199bc1607596914beb1710f5087e4f881c
sjd-c <uxsalua@gmail.com>
2026-07-25 10:57:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'youscoin-v2';
+const CACHE = 'youscoin-v3';
 const SHELL = ['./index.html', './host/index.html', './host/setup.html', './manifest.json'];
 
 self.addEventListener('install', e => {
